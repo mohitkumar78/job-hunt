0dd69f7752a49f6955d522b5117a151b0093a8e9
mohitkumar78 <129609791+mohitkumar78@users.noreply.github.com>
2024-09-17 15:06:46 +0530

    Your commit message here

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>{" "}
+      <Button>jgjh</Button>
+    </>
+  );
+}
+
+export default App;
