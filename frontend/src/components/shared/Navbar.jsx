7208daf64c9c794be22ae4549f416c2114476074
mohitkumar78 <129609791+mohitkumar78@users.noreply.github.com>
2024-10-22 16:16:15 +0530

    fix bug

--- frontend/src/components/shared/Navbar.jsx
+++ frontend/src/components/shared/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"; // Ensure AvatarFallback is imported
-import { Button } from "@/components/ui/Button";
+import { Button } from "../ui/button";
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 
