cf66969041bdae00810137c4620dc90b4d44242b
mohitkumar78 <129609791+mohitkumar78@users.noreply.github.com>
2025-03-02 16:56:14 +0530

    bug and layout changing and depolyement

--- backend/index.js
+++ backend/index.js
@@ -7,17 +7,17 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
-
+import path from "path"
 dotenv.config({});
 
 const app = express();
-
+const _dirname = path.resolve()
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'http://localhost:5174',
+    origin: 'http://localhost:5173',
     credentials: true
 }
 
@@ -32,8 +32,10 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-
-
+app.use(express.static(path.join(_dirname, "/frontend/dist")))
+app.get("*", (req, res) => {
+    res.sendFile(_dirname, "frontend", "dist", "index.html")
+})
 app.listen(PORT, () => {
     connectDB();
     console.log(`Server running at port ${PORT}`);
